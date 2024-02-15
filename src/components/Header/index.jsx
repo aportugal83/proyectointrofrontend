33c61e9673a51050e20799091b11266f4a78b035
aportugal83 <aportugal.lc@gmail.com>
2024-02-15 15:51:21 -0800

    se implemento navlink en el header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
 import './header.css'
 
 
 const Header = () => {
+//aqui se agrega una funcion flecha complemento del NavLink de router dom
+//condicion ternaria verdadero ?
+const linkIsActive = (isActive) => {
+  return isActive ? 'header-list-link is-active' : 'header-list-link';
+}
 
+  //el navlink sustituye a la etiqueta <a></a>
   return (
     <header>
-        <nav className="header">
-            <a className="header-logo" href="#">Logo NETCHILL</a>
-            <ul className="header-nav-list">
-                <li>
-                  <a className="header-list-link" href="/">Home</a>
-                </li>
-                <li>
-                  <a className="header-list-link" href="/about">About</a>
-                  </li>
-                <li>
-                  <a className="header-list-link is-active" href="/contact">Contact</a>
-                </li>
-            </ul>
-        </nav>
+      <nav className="header">
+        
+        <NavLink to="/" className="header-logo">Logo NETCHILL</NavLink>
+
+        <ul className="header-nav-list">
+          <li>
+            <NavLink to="/" className={({isActive}) => linkIsActive(isActive)}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className={({isActive}) => linkIsActive(isActive)}>
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="contact" className={({isActive}) => linkIsActive(isActive)}>
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
 
   )
